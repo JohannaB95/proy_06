315b504e4656898f3290d566693c4ccae1669ea0
JohannaB95 <johanna_b_g@hotmail.com>
2024-01-20 18:44:06 -0500

    Cambios en documentación

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
-//Se declara require para importar colors, desde la carpeta node_modules.
-require ('colors');
+//Se carga la libreria colors para permitir el uso de estilos de colores en la terminal
+require('colors');
 
-/*se declara una función const de nombre math para que importe de la carpeta proy_module las funciones.
-y operaciones que se ejecutaran*/
+//Se declara una variable de tipo constante llamada math que almacena los datos del módulo solicitado
+//Se carga el código que contiene el archivo oper_math.js que se encuentra en la carpeta modules.
 const math = require('./proy_modules/math.js')
 
-// Se declara una función flecha const main, que es asincrónica
+// Se declara una función flecha constante llamada main, que es asincrónica
 const main = async () => {
 
-    /*Se solicita imprimir en la consola el mensaje que esta entre paréntesis, que añada una línea de espacio
-    y que el texto sea de color rojo y se añade diseño a la interfaz*/
+    //Se imprime en la consola un mensaje, que el texto sea de color rojo y se añade diseño a la interfaz
     console.log(`*****************`.cyan);
     console.log(`* `.cyan + 'hola SENA CBA'.red + ` *`.cyan);
     console.log(`*****************\n`.cyan);
 
-     //Se declara una variable constante llamada numeros a la que se le asigna un arreglo de numeros
+    //Se declara una variable constante llamada numeros que se inicializa con un arreglo de numeros
     const numeros = [1, 2, 800, 912, 1000, 1283, 2811];
 
-     /* Se utiliza for para crear un bucle que se ejecute con todos los argumentos dados en la constante 
-    numeros, cada vez que este realice la iteración el valor se asigna en numero.*/
-    for (const numero of numeros){
+    /* Se utiliza for para crear un bucle que se ejecute con todos los argumentos dados en la constante 
+   numeros, cada vez que este realice la iteración el valor se asigna en numero.*/
+    for (const numero of numeros) {
 
-    /* Se declara una variable constante llamada invertidoComoNumero que estara exportando del módulo
-    math los parámetros requeridos.*/
+        /* Se declara una variable constante llamada invertidoComoNumero que estara exportando del módulo
+        math el código allí contenido.*/
         const invertidoComoNumero = math.invertirNumero(numero);
 
-    /* Se declara una variable constante llamada invertidoComoCadena que estara exportando del módulo
-    math los parámetros requeridos.*/
+        /* Se declara una variable constante llamada invertidoComoCadena que estara exportando del módulo
+        math el código allí contenido.*/
         const invertidoComoCadena = math.invertirNumeroComoCadena(numero);
 
-     /*Se solicita imprimir en la consola el resultado de invertir un número como número y como cadena,
-    y que el texto dado como resultado sea de color azul y se le añade diseño a la interfaz*/
+        /*Se solicita imprimir en la consola el resultado de invertir un número como número y como cadena,
+       y que el texto dado como resultado sea de color azul y se le añade diseño a la interfaz*/
         console.log(`****************************************************************************`.magenta);
         console.log(`*  `.magenta + "El número '%s' es '%s' invertido como número, y '%s' como cadena" + `  *`.magenta,
-        numero, invertidoComoNumero, invertidoComoCadena );
+            numero, invertidoComoNumero, invertidoComoCadena);
         console.log(`****************************************************************************\n`.magenta);
     }
 
-    // Se declara una variable constante llamada textos, a la cual se la ha asignado un arreglo de strings.
+    // Se declara una variable constante llamada textos, que se inicializa con un arreglo de strings.
     const textos = ['Hola Sena', 'Ficha 2798618', 'Análisis y desarrollo de software']
 
-     /* Se utiliza for para crear un bucle que se ejecute con todos los argumentos dados en la constante 
-    textos, cada vez que este realice la iteración el valor se asigna en texto.*/
-    for(const texto of textos){
+    /* Se utiliza for para crear un bucle que se ejecute con todos los argumentos dados en la constante 
+   textos, cada vez que este realice la iteración el valor se asigna en texto.*/
+    for (const texto of textos) {
 
-    /* Se declara una variable constante llamada textoInvertido que estara exportando del módulo
-    math los parámetros requeridos.*/
+        /* Se declara una variable constante llamada textoInvertido que estara exportando del módulo
+        math el código allí contenido.*/
         const textoInvertido = math.invertirTexto(texto);
-    
-    /*Se solicita imprimir en la consola el resultado de invertir el texto dado y que el texto resultado
-     sea de color rojo.*/
+
+        //Se solicita imprimir en la consola el texto inicial y el resultado de la inversión
         console.log(`****************************************************************************************************`.yellow);
         console.log(`* `.yellow + "El texto '%s' se invierte como '%s'" + `*`.yellow, texto, textoInvertido);
         console.log(`****************************************************************************************************\n`.yellow);
@@ -57,18 +55,17 @@ const main = async () => {
 
     /*Se declara una variable constante llamada datos, a la cual se le ha asignado un arreglo con tres elementos
     que son un number, un string y otro arreglo */
-        const datos = [1234567, 'ADSO 2798618', [2798618, 'ADSO']]
+    const datos = [1234567, 'ADSO 2798618', [2798618, 'ADSO']]
 
     /* Se utiliza for para crear un bucle que se ejecute con todos los argumentos dados en la variable let 
     datos, cada vez que este realice la iteración el valor se asigna en dato.*/
-    for(let dato of datos){
+    for (let dato of datos) {
 
-          /* Se declara una variable constante llamada datoInvertido que estara exportando del módulo
-    math los parámetros requeridos.*/
+        /* Se declara una variable constante llamada datoInvertido que estara exportando del módulo
+        math el código allí contenido.*/
         const datoConvertido = math.conversionDatos(dato);
 
-         /*Se solicita imprimir en la consola el resultado de la conversión del dato dado y que el texto resultado
-     sea de color amarillo.*/
+        //Se solicita imprimir en la consola el resultado de la conversión de cada dato.
         console.log(`**************************************************`.red);
         console.log(`*  `.red + `El resultado de conversión es : ${datoConvertido}` + `  *`.red);
         console.log(`**************************************************\n`.red);
@@ -77,4 +74,4 @@ const main = async () => {
 }
 
 //Se invoca la función main
-main( );
+main();
--- proy_modules/math.js
+++ proy_modules/math.js
@@ -1,19 +1,18 @@
-//Se declara una variable constante llamada math y que es un objeto.
+//Se declara una variable constante llamada math y que se inicializa con un objeto vacio.
 const math = {};
 
-/* Se esta declarando una variable constante llamada invertirNumero y que es una función flecha y que
-tendrá un parámetro numero que se invertira.*/
+/* Se declara una variable constante llamada invertirNumero y que es una función flecha y que
+tiene como parámetro a numero.*/
 const invertirNumero = numero => { 
 
- //Se declara una variable let llamada invertido y que tiene como valor 0. 
+ //Se declara una variable let llamada invertido y que se inicializa en 0. 
     let invertido = 0;
 
-   /* Se esta creando un bucle que tiene como condicion en la que se dice que  numero debe ser 
-   diferente a 0 */
+   // Se crea un bucle while que tiene como condición que numero debe ser diferente a 0
     while (numero !=0){
 
-        /* Se esta declarando que invertido será igual a la operación de multiplicar 10 por el valor de invertido
-        más el resultado de el residuo del valor del residuo de numero entre 10*/
+        /* Se multiplica el número invertido por 10, lo cual "mueve" sus dígitos un lugar hacia la izquierda.
+        Se suma el último dígito del número original (numero % 10) al número invertido.*/
         invertido = 10 * invertido + numero % 10
 
         /* Se declara que se va a dividir el valor de numero entre 10 y que el resultado se redondeara en un 
@@ -21,55 +20,56 @@ const invertirNumero = numero => {
         numero = (Math.floor(numero / 10))
     }
 
-    /* Se solicita que se devuelva el valor de la variable invertido */
+    // Se retorna el valor de la variable invertido
     return invertido
 };
 
-/* Se esta declarando una variable constante llamada invertirNumeroComoTexto y que es una función
+/* Se declara una variable constante llamada invertirNumeroComoTexto y que es una función
 flecha, que tendrá un parámetro numero que se invertira como texto.*/
 const invertirNumeroComoTexto = numero => {
 
-
- /*Se esta declarando una variable constante llamada numeroInvertidoComoTexto que convertirá un número
+ /*Se declara una variable constante llamada numeroInvertidoComoTexto que convertirá un número
  a string e invertira su orden.*/
     const numeroInvertidoComoTexto = numero.toString().split("").reverse().join("");
     
-    /* Se solicita que se devuelva el valor de numeroInvertidoComoTexto */
+    /* Se retorna el valor de numeroInvertidoComoTexto */
     return numeroInvertidoComoTexto;
 };
 
-/* Se esta declarando una variable constante llamada invertirTexto y que es una función
-flecha, que tendrá un parámetro texto que se invertira como texto.*/
+/* Se declara una variable constante llamada invertirTexto, que es una función flecha y que tendrá un 
+parámetro texto que se invertira como texto.*/
 const invertirTexto = texto => {
 
-    //Se declara que se dividira una cadena de texto y se separara con un espacio.
+    /*Se declara una variable de tipo de constante llamada splitText que almacena el resultado de dividir una 
+    cadena de texto y separarla con un espacio.*/
     const splitText = texto.split('');
 
-    //Se declara que se va a revertir el orden de los  elementos del texto.
+    /*Se declara una variable constante llamada reversedText que almacena el resultado de revertir el orden de
+    los  elementos del texto.*/
     const reversedText = splitText.reverse( );
 
-    /*Se declara que los elementos que se separaron en splitText se uniran en una cadena y el resultado
-    quedara invertido*/
+    /*Se declara una variable constante llamada joinedText que almacena el resultado de la unión de los elementos
+    revertidos anteriormente*/
     const joinedText = reversedText.join('');
 
-    /* Se solicita que se devuelva el valor de joinedTexto*/
+    /* Se retorna el valor de joinedTexto*/
     return joinedText;
 };
 
-/* Se esta declarando una variable constante llamada invertirArreglo y que es una función
+/*Se declara una variable constante llamada invertirArreglo y que es una función
 flecha, que tendrá un parámetro arreglo que se invertira como objeto.*/
 const invertirArreglo = arreglo => {
 
-     /*Se declara que se revertira un arreglo y que se guardara el resultado en la variable constante
-     reverseObject*/
+     /*Se declara una variable constante llamada reverseObject, que revertira un arreglo y el resultado se 
+     guardara en la variable*/
      const reverseObject = arreglo.reverse();
 
-    /* Se solicita que se devuelva el valor de reverseObject*/
+    /* Se retorna el valor de reverseObject*/
     return reverseObject;
 };
 
-/* Se esta declarando una variable constante llamada conversionDatos y que es una función
-flecha, que tendrá un parámetro*/
+/*Se declara una variable constante llamada conversionDatos y que es una función flecha, que tiene como
+parámetro a data*/
 const conversionDatos = data => {
    
     /*Se declara una variable llamada dataType y que por medio de la expresión typeof estará determinando
@@ -79,43 +79,50 @@ const conversionDatos = data => {
     //Se declara una variable llamada dataVar que guardara el valor de cada uno de los tipos de datos.
     let dataVar;
 
-/* Se declara una condición en la que se especifica que si alguno de los parámetros dados cumple con 
-ser igual a un espacio, null, undefined o su longitud es igual a 0 se obtendrá un mensaje que dice 'El 
-valor ingresado está vacio o incorrecto' */
+/*Se usa un condicional if que tiene como condición que si alguno de los parámetros es igual a un strin vacio,
+null, undefined o su longitud es igual a 0 se obtendrá un mensaje que dice 'El valor ingresado está vacio o 
+incorrecto' */
     if(data === '' || data === null || data === undefined || data.length === 0) {
         dataVar = 'El valor ingresado está vacio o incorrecto';
     
-    /* Se declara que si la condición anterior no se cumple, se procedera a evaluar diferentes opciones 
-    (switch), que en el caso (case) de cumplir con el tipo de dato que se esta especificando se imprimara
-    un mensaje en el que se informa que el tipo de valor ingresado es de tipo number, string u object y el 
-    valor que tiene asignado, sino cumple con ninguna condición saldrá de esta por medio de (break) y pasara 
-    a la siguiente. Si al final ninguna de las condiciones se cumple el valor de dataVar será (default), mostrará 
-    un mensaje que diga que ' El valor ingresado no se puede invertir' */
+    //Si la condición anterior no se cumple: 
     }else {
+        //Se imprime en la consola un mensaje indicando de que tipo de dato se trata y su valor
         console.log(`****************************************************************************`.blue);
         console.log(`*   `.blue + `El valor ingresado es de tipo: ${dataType}, con un valor de: ${data}`  + `   *`.blue);
         console.log(`****************************************************************************\n`.blue);
+        //Se usa un switch que recibira como parámetro a dataType
         switch(dataType){
+            //En el caso de que el dato sea de tipo de number
             case 'number' :
+                //Se imprime en la consola el mensaje indicando el tipo de dato y su valor
                 dataVar = invertirNumero(data);
+                //Se sale del caso
                 break;
+            //En el caso de que el dato sea de tipo de string
             case 'string' :
+                //Se imprime en la consola el mensaje indicando el tipo de dato y su valor
                  dataVar = invertirTexto(data);
+                 //Se sale del caso
                 break;
+            //En el caso de que el dato sea de tipo de object
             case 'object' :
+                //Se imprime en la consola el mensaje indicando el tipo de dato y su valor
                 dataVar = invertirArreglo(data);
+                //Se sale del caso
                 break;
+            //Si ninguno de los casos anteriores se cumple
             default:
+                //Se imprime en la consola el siguiente mensaje
                 dataVar = 'El valor ingresado no se puede invertir';
             }
         }
-    /* Se solicita que se devuelva el valor de dataVar*/
+    /* Se retorna el valor de dataVar*/
     return dataVar;
 }
 
-
-/*Se declaran que las funciones que están después del igual tienen propiedades, operaciones, 
-condiciones, etc, que se podrán importar al módulo que las requiere invocando la palabras math. y el
+/*Se asignan cada una de las funciones como propiedades del objeto Math. Esto crea métodos asociados con
+el objeto Math, y que se podrán importar al módulo que las requiere invocando la palabras math. y el
 nombre de cada una de estas.*/
 math.invertirNumero = invertirNumero;
 math.invertirNumeroComoCadena = invertirNumeroComoTexto;
